7e6ea81b86c7594b81a9508655a827d103699fea
Scott Davies <scottdavies1983@gmail.com>
2022-12-04 21:18:06 +0000

    set header tablet

--- common/components/app/Header.js
+++ common/components/app/Header.js
@@ -6,12 +6,15 @@ import logo from "../../../public/assets/images/shared/desktop/logo.svg";
 const Header = () => {
   return (
     <div className={"h-24  bg-otherBlack flex w-full "}>
-      <div className="flex h-full w-full justify-between  minDesktop:pt-8 minDesktop:mx-41 minDesktop:border-b minDesktop:border-white minDesktop:border-opacity-20  ">
+      <div className="flex h-full w-full justify-between  minTablet:pt-8 minTablet:mx-9.5  minDesktop:mx-41 minTablet:border-b minTablet:border-white minTablet:border-opacity-20  ">
         <div className="mr-[15%]">
-          <Image src={logo} alt="cart" />
+          <p className="minDesktop:hidden text-white inline-block minTablet:mr-10.5">
+            hb
+          </p>
+          <Image src={logo} alt="cart" className="inline-block" />
         </div>
 
-        <div className="flex basis-98 tracking-twopx leading-[1.56rem] font-manReg text-SUB justify-between text-white mr-auto">
+        <div className="hidden minDesktop:flex basis-98 tracking-twopx leading-[1.56rem] font-manReg text-SUB justify-between text-white mr-auto">
           <div>HOME</div>
           <div>HEADPHONES</div>
           <div>SPEAKERS</div>

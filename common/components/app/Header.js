2637cd9e2a39c53921ae697a72cd3287cca4e039
Scott Davies <scottdavies1983@gmail.com>
2022-12-06 06:21:23 +0000

    Saved next to the girls demo

--- common/components/app/Header.js
+++ common/components/app/Header.js
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import cart from "../../../public/assets/images/shared/desktop/icon-cart.svg";
 import logo from "../../../public/assets/images/shared/desktop/logo.svg";
+import burger from "../../../public/assets/images/shared/tablet/icon-hamburger.svg";
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <div className={"h-24  bg-otherBlack flex w-full "}>
-      <div className="flex h-full w-full justify-between  minTablet:pt-8 minTablet:mx-9.5  minDesktop:mx-41 minTablet:border-b minTablet:border-white minTablet:border-opacity-20  ">
-        <div className="mr-[15%]">
-          <p className="minDesktop:hidden text-white inline-block minTablet:mr-10.5">
-            hb
-          </p>
-          <Image src={logo} alt="cart" className="inline-block" />
-        </div>
+    <>
+      <div
+        className={"h-24  bg-otherBlack minTablet:bg-opacity-0 flex w-full "}
+      >
+        <div className="flex h-full w-full justify-between mx-6 pt-8  minTablet:mx-9.5  minDesktop:mx-41 minTablet:border-b minTablet:border-white minTablet:border-opacity-20  ">
+          <div className="minTablet:hidden text-white">
+            <Image
+              src={burger}
+              alt={"dropdown"}
+              onClick={() => setMenu(!menu)}
+              className={"inline-block"}
+            />
+          </div>
 
-        <div className="hidden minDesktop:flex basis-98 tracking-twopx leading-[1.56rem] font-manReg text-SUB justify-between text-white mr-auto">
-          <div>HOME</div>
-          <div>HEADPHONES</div>
-          <div>SPEAKERS</div>
-          <div>EARPHONES</div>
-        </div>
+          <div className="minTablet:mr-[15%]">
+            <Image
+              src={burger}
+              alt={"dropdown"}
+              onClick={() => setMenu(!menu)}
+              className="hidden mintablet:visible minDesktop:hidden text-white minTablet:inline-block minTablet:mr-10.5"
+            />
 
-        <div className="text-grey">
-          <Image src={cart} alt="cart" />
+            <Image src={logo} alt="cart" className="inline-block" />
+          </div>
+
+          <div className="hidden minDesktop:flex basis-98 tracking-[2px] leading-[1.56rem] font-manReg text-SUB justify-between text-white mr-auto">
+            <div>HOME</div>
+            <div>HEADPHONES</div>
+            <div>SPEAKERS</div>
+            <div>EARPHONES</div>
+          </div>
+
+          <div className="text-grey">
+            <Image src={cart} alt="cart" />
+          </div>
         </div>
       </div>
-    </div>
+      <div className={`${!menu && "hidden"} fixed top-30 bg-grey w-full h-40`}>
+        Hello im a placeholder
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import hero from "../public/assets/images/home/desktop/image-hero.jpg";
 
 export default function Home() {
-  return <div className="bg-orange">Im the home page</div>;
+  return (
+    <div className="">
+      <Image
+        className={"fixed top-0 -z-20"}
+        src={hero}
+        alt={"headphones hero"}
+      />
+      <div className="text-white w-97 minDesktop:fixed minDesktop:top-57 minDesktop:left-41">
+        <p
+          className={"font-manReg text-OVERLINE leading-[19px] tracking-[10px]"}
+        >
+          NEW PRODUCT
+        </p>
+        <p className="font-manBold text-H1 tracking-[2px] leading-[3.625rem]">
+          XX99 MARK II HEADPHONES
+        </p>
+        <p>
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <h1>See product</h1>
+      </div>
+    </div>
+  );
 }
